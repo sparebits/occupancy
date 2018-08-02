cdbf8b8c3c80f9937710eb5ac228a741f353c02f
neiko.neikov <neiko.neikov@hubparking.com>
2018-08-02 09:33:09 +0300

    working simple spring boot application

---
+++ src/main/java/com/faac/occupancy/Application.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.faac.occupancy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author nneikov
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
