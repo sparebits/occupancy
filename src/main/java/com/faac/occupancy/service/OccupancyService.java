9190f20ef0b57601fb0d2bbb72825030e8fac6be
neiko.neikov <neiko.neikov@hubparking.com>
2018-08-06 09:45:09 +0300

    notifying browser for incoming occuoancy information

--- src/main/java/com/faac/occupancy/service/OccupancyService.java
+++ src/main/java/com/faac/occupancy/service/OccupancyService.java
@@ -2,6 +2,9 @@ package com.faac.occupancy.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +16,14 @@ import com.faac.occupancy.model.Response;
 public class OccupancyService {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
+    
+    @Autowired
+    private SimpMessagingTemplate template;
 
     @RequestMapping(value = "/occupancy", method = RequestMethod.PUT)
-    public Response pushOccupancy(Occupancy occupancy) {
+    public Response pushOccupancy(@RequestBody Occupancy occupancy) {
         logger.info("pushing occupancy...");
+        template.convertAndSend("/topic/occupancy", occupancy);
         return new Response();
     }
 
